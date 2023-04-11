d9d3776ea6d8e29eb0b7f8070dde982f8b267183
seafoodude <michaelhuang777@gmail.com>
2023-04-11 16:27:58 -0700

    wrote server.js and reformatted the files

---
+++ server.js
@@ -0,0 +1,76 @@
+// Import Dependencies
+const express = require('express');
+const path = require("path")
+const fs = require("fs")
+const util = require("util")
+
+// Hanlding Async
+const readFileAsync = util.promisify(fs.readFile)
+const writeFileAsync = util.promisify(fs.writeFile)
+
+//establishing Server
+const app = express()
+const PORT = process.env.PORT || 8000
+
+app.use(express.urlencoded({ extended:true }))
+app.use(express.json())
+
+//Static Middleware
+app.use(express.static("./develop/public"))
+
+//Get request thru API route
+app.get("/api/notes", function(req, res) {
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes)
+    })
+})
+
+//POST request
+app.post("/api/notes", function(req, res) {
+    const note = req.body
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data){
+        const notes = [].concat(JSON.parse(data))
+        note.id = notes.length + 1
+        notes.push(note)
+        return notes
+    }).then(function(notes) {
+        writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+        res.json(note)
+    })
+})
+
+//Delete request
+app.delete("/api/notes/:id", function(req, res){
+    const idToDelete = parseInt(req.params.id)
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        const notes = [].concat(JSON.parse(data))
+        const newNotesData = []
+        for (let i = 0; i<notes.length; i++) {
+            if(idToDelete !== notes[i].id) {
+                newNotesData.push(notes[i])
+            }
+        }
+        return newNotesData
+    }).then(function(notes) {
+        writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+        res.send('saved successfully')
+    })
+})
+
+//HTML routes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/notes.html"))
+})
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"))
+})
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"))
+})
+
+app.listen(PORT, function() {
+    console.log(`app listening at: http://localhost:${PORT}`)
+})
