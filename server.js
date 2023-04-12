a2ef83b0f2f33e4cbd140a0d9a8f89525523189f
seafoodude <michaelhuang777@gmail.com>
2023-04-12 14:48:03 -0700

    heroku

--- server.js
+++ server.js
@@ -60,11 +60,11 @@ app.delete("/api/notes/:id", function(req, res){
 
 //HTML routes
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"))
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
 })
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/Develop/public/index.html"))
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
 })
 
 app.listen(PORT, function() {
