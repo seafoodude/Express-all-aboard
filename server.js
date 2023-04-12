7b34ef836d49d46892f9af5c3c42695a4d63999a
seafoodude <michaelhuang777@gmail.com>
2023-04-12 14:35:32 -0700

    fixing code up for heroku

--- server.js
+++ server.js
@@ -63,10 +63,6 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/develop/public/notes.html"))
 })
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/develop/public/index.html"))
-})
-
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/develop/public/index.html"))
 })
